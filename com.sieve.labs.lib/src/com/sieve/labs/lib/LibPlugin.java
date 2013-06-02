3f3d711b4f2cd1068c38540c2c9a5be3413c5c19
loneranger <anubhav.sahoo@gmail.com>
2013-06-03 00:58:12 +0530

    library plugin

---
+++ com.sieve.labs.lib/src/com/sieve/labs/lib/LibPlugin.java
@@ -0,0 +1,35 @@
+package com.sieve.labs.lib;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class LibPlugin implements BundleActivator {
+	public static final String PLUGIN_ID = "com.sieve.labs.lib";
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		LibPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		LibPlugin.context = null;
+	}
+
+}
